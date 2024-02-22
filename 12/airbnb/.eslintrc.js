59afdf4a96c58ee332f8be7439ca6495ac553565
Jeff Passetti <jpassetti@gmail.com>
2024-02-22 18:24:47 -0500

    added airbnb from tonight's class

---
+++ 12/airbnb/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended', // Add this line
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // You can add or override specific rules here
+  },
+}
